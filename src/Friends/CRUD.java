2a9b7c4d9533d37243f51f6344e0a305d086d399
SantyMejia20 <saamejiaga@unal.edu.co>
2024-09-11 16:00:37 -0500

    create

---
+++ src/Friends/CRUD.java
@@ -0,0 +1,114 @@
+package Friends;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class CRUD {
+    private File file;
+
+    public CRUD() {
+        file = new File("friendsContact.txt");
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String addFriend(String newName, long newNumber) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        String mensaje = "Contacto agregado";
+
+        while (raf.getFilePointer() < raf.length()) {
+            String[] lineSplit = raf.readLine().split("!");
+
+            if (lineSplit.length == 2 && lineSplit[0].equals(newName) || Long.parseLong(lineSplit[1]) == newNumber) {
+                mensaje = "Ya existe este contacto";
+            }else{
+                raf.writeBytes(newName + "!" + newNumber);
+                raf.writeBytes(System.lineSeparator());
+            }
+        }
+        return mensaje;
+    }
+
+    public Long readFriend(String searchName) throws IOException {
+
+        RandomAccessFile raf = new RandomAccessFile(file, "r");
+
+        while (raf.getFilePointer() < raf.length()) {
+            String[] lineSplit = raf.readLine().split("!");
+
+            if (lineSplit.length == 2 && lineSplit[0].equals(searchName)) {
+                return Long.parseLong(lineSplit[1]);
+            }
+        }
+        return null;
+    }
+
+    public String updateFriend(String newName, long newNumber) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        File tmpFile = new File("temp.txt");
+        RandomAccessFile tmpraf = new RandomAccessFile(tmpFile, "rw");
+        String mensaje = "Contacto no existe";
+
+        while (raf.getFilePointer() < raf.length()) {
+            String[] lineSplit = raf.readLine().split("!");
+
+            if (lineSplit.length == 2 && lineSplit[0].equals(newName)) {
+                mensaje = "Contacto actualizado";
+                tmpraf.writeBytes(newName + "!" + newNumber + System.lineSeparator());
+            } else {
+                tmpraf.writeBytes(raf.readLine() + System.lineSeparator());
+            }
+        }
+
+        raf.setLength(0);
+        tmpraf.seek(0);
+
+        while (tmpraf.getFilePointer() < tmpraf.length()) {
+            raf.writeBytes(tmpraf.readLine() + System.lineSeparator());
+        }
+
+        raf.close();
+        tmpraf.close();
+        tmpFile.delete();
+
+        return mensaje;
+    }
+
+    public String deleteFriend(String nameToDelete) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(file, "rw");
+        File tmpFile = new File("temp.txt");
+        RandomAccessFile tmpraf = new RandomAccessFile(tmpFile, "rw");
+        String mensaje = "El contacto no existe";
+
+        while (raf.getFilePointer() < raf.length()) {
+            String[] lineSplit = raf.readLine().split("!");
+
+            if (lineSplit.length == 2 && lineSplit[0].equals(nameToDelete)) {
+                mensaje = "Contacto eliminado";
+                continue;
+            }
+
+            tmpraf.writeBytes(raf.readLine() + System.lineSeparator());
+        }
+
+        raf.seek(0);
+        tmpraf.seek(0);
+
+        raf.setLength(0);
+        while (tmpraf.getFilePointer() < tmpraf.length()) {
+            raf.writeBytes(tmpraf.readLine() + System.lineSeparator());
+        }
+
+        raf.close();
+        tmpraf.close();
+        tmpFile.delete();
+
+        return mensaje;
+    }
+}
---
+++ src/Friends/VentanaPrincipal.java
@@ -0,0 +1,183 @@
+package Friends;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class VentanaPrincipal extends JFrame implements ActionListener {
+
+    private Container contenedor;
+    private JLabel nombre, numero;
+    private JTextField campoNombre, campoNumero;
+    private JButton create, read, update, delete, clear;
+
+    public VentanaPrincipal() {
+        inicio();
+        setTitle("Contactos");
+        setSize(280,210);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    private void inicio(){
+        contenedor = getContentPane();
+        contenedor.setLayout(null);
+
+        nombre = new JLabel();
+        nombre.setText("Nombre: ");
+        nombre.setBounds(20, 20, 135, 23);
+        campoNombre = new JTextField();
+        campoNombre.setBounds(100, 20, 150, 23);
+
+        numero = new JLabel();
+        numero.setText("Número: ");
+        numero.setBounds(20, 50, 135, 23);
+        campoNumero = new JTextField();
+        campoNumero.setBounds(100, 50, 150, 23);
+
+        create = new JButton();
+        create.setText("Create");
+        create.setBounds(10, 80, 75, 23);
+        create.addActionListener(this);
+
+        read = new JButton();
+        read.setText("Read");
+        read.setBounds(95, 80, 75, 23);
+        read.addActionListener(this);
+
+        update = new JButton();
+        update.setText("Update");
+        update.setBounds(180, 80, 75, 23);
+        update.addActionListener(this);
+
+        delete = new JButton();
+        delete.setText("Delete");
+        delete.setBounds(55, 113, 75, 23);
+        delete.addActionListener(this);
+
+        clear = new JButton();
+        clear.setText("Clear");
+        clear.setBounds(140, 113, 75, 23);
+        clear.addActionListener(this);
+
+        contenedor.add(nombre);
+        contenedor.add(campoNombre);
+        contenedor.add(numero);
+        contenedor.add(campoNumero);
+        contenedor.add(create);
+        contenedor.add(read);
+        contenedor.add(update);
+        contenedor.add(delete);
+        contenedor.add(clear);
+    }
+
+    public void actionPerformed(ActionEvent event){
+        if (event.getSource() == create){
+            String nombre = campoNombre.getText();
+            Long numero = Long.parseLong(campoNumero.getText());
+            CRUD crud = new CRUD();
+            String mensaje = null;
+            try {
+                mensaje = crud.addFriend(nombre, numero);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            JOptionPane.showMessageDialog(contenedor, mensaje);
+
+        }
+
+        if (event.getSource() == read){
+           // read();
+        }
+
+        if (event.getSource() == update){
+            //update();
+        }
+
+        if (event.getSource() == delete){
+            //delete();
+        }
+
+        if (event.getSource() == clear){
+            campoNombre.setText("");
+            campoNumero.setText("");
+        }
+    }
+
+    /*
+    public void create(){
+        boolean error = false;
+        String nombre;
+        Long numero;
+        try {
+            nombre = campoNombre.getText();
+            numero = Long.parseLong(campoNumero.getText());
+            CRUD crud = new CRUD();
+            String mensaje = crud.addFriend(nombre, numero);
+            JOptionPane.showMessageDialog(contenedor, mensaje);
+        } catch (Exception e){
+            error = true;
+        } finally {
+            if (error) {
+                JOptionPane.showMessageDialog(null,"Debes ingresar un número válido", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void read(){
+        boolean error = false;
+        String nombre;
+        try {
+            nombre = campoNombre.getText();
+            CRUD crud = new CRUD();
+            crud.readFriend(nombre);
+        } catch (Exception e){
+            error = true;
+        } finally {
+            if (error) {
+                JOptionPane.showMessageDialog(null,"Debes ingresar un número", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void update(){
+        boolean error = false;
+        String nombre;
+        Long numero;
+        try {
+            nombre = campoNombre.getText();
+            numero = Long.parseLong(campoNumero.getText());
+            CRUD crud = new CRUD();
+            crud.updateFriend(nombre, numero);
+        } catch (Exception e){
+            error = true;
+        } finally {
+            if (error) {
+                JOptionPane.showMessageDialog(null,"Debes ingresar un número", "Error", JOptionPane.ERROR_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(null,"Debes ingresar un número", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public void delete(){
+        boolean error = false;
+        String nombre;
+        try {
+            nombre = campoNombre.getText();
+            CRUD crud = new CRUD();
+            crud.deleteFriend(nombre);
+        } catch (Exception e){
+            error = true;
+        } finally {
+            if (error) {
+                JOptionPane.showMessageDialog(null,"Debes ingresar un número", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+
+*/
+}
